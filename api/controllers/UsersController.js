f8be9e981dff09fc6094dcf2069642bb8f5940ba
jsmith312 <jts312@msn.com>
2017-04-01 21:09:50 -0700

    init commit

---
+++ api/controllers/UsersController.js
@@ -0,0 +1,25 @@
+/* global Users */
+/**
+ * UsersController
+ */
+
+module.exports = {
+	create(req, res) {
+		Users.add(req.allParams(), (err, user) => {
+			if (err) {
+				return res.negotiate(err);
+			}
+			return res.created({ user });
+		});
+	},
+
+	getAll(req, res) {
+		Users.find().exec((err, users) => {
+			if (err) {
+				return res.negotiate(err);
+			}
+			console.log(users);
+			return res.ok({ users });
+		});
+	},
+};
